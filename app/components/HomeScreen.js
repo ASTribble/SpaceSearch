4c62443cd12479dcd10cea7e0a81d94f2e6113c1
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-22 15:11:01 -0700

    added HomeContainer

---
+++ app/components/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {connect, dispatch} from 'react-redux';
+import {withNavigation} from 'react-navigation';
+import { bindActionCreators} from 'redux';
+import {
+  Button,
+  View,
+  Text,
+  Image,
+  TextInput
+} from 'react-native';
+
+import * as Actions from '../actions/index';
+
---
+++ app/containers/HomeContainer.js
@@ -0,0 +1,16 @@
+import {connect} from 'react-redux';
+import { HomeScreen } from '../components/HomeScreen';
+
+
+const mapStateToProps = (state, props) => {
+    return {
+      loading: state.spaceReducer.loading,
+      searchTerm: state.searchReducer.searchTerm
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withNavigation(HomeScreen));
