1e934d530ada59786fbe9c9da6507407fbb0abc3
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-22 15:58:20 -0700

    added HomeScreen Component

--- app/components/HomeScreen.js
+++ app/components/HomeScreen.js
@@ -10,5 +10,40 @@ import {
   TextInput
 } from 'react-native';
 
-import * as Actions from '../actions/index';
 
+class HomeScreen extends React.Component {
+
+  static navigationOptions = ({ navigation}) => {
+    const params = navigation.state.params || {};
+    return ({
+      title: 'Home'
+    });
+  }
+
+  render() {
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Sometimes, we just need a little perspective...</Text>
+        <Image
+          source={{ uri: "https://images-assets.nasa.gov/image/PIA00342/PIA00342~small.jpg" }}
+          style={{ height: 450, width: 350 }}
+        />
+        <TextInput
+          style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, marginTop: 10 }}
+          onChangeText={(text) => this.props.setSearchTerm(text)}
+          value={this.props.searchTerm}
+        />
+        <Button
+          title='Find It'
+          onPress={() => {
+            this.props.fetchSpaceImages(this.props.searchTerm);
+            this.props.navigation.navigate('SearchResults', {
+              subject: 'Search Results',
+            })
+          }}
+        />
+      </View>
+    );
+  }
+}
